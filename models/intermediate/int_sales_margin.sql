61868a4b5192909a334c6ca54be298379f6df30f
ezzat_o@icloud.com <ezzat_o@icloud.com>
2026-07-11 15:32:16 +0000

    amendment

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,4 +5,5 @@ select
     (sa.quantity * pdt.purchase_price) as purchase_cost,
     (sa.revenue - (sa.quantity * pdt.purchase_price)) as margin
 from {{ ref("stg_raw_sales") }} as sa
-left join {{ ref("stg_raw_product") }} as pdt on sa.products_id = pdt.products_id
+left join {{ ref("stg_raw_product") }} as pdt 
+on sa.products_id = pdt.products_id
