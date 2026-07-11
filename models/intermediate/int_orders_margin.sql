d9ccc5799674ef2b4cd6a8cda4de335ccbfbf4e6
ezzat_o@icloud.com <ezzat_o@icloud.com>
2026-07-11 17:39:27 +0000

    amend the code of int_order_margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,6 +1,14 @@
-SELECT *
+SELECT 
+orders_id,
+SUM(revenue) AS total_revenue,
+date_date,
+SUM(quantity) AS quantity,
+SUM(purchase_cost) AS purchase_cost
+SUM(margin) AS margin
 FROM 
 {{ ref('int_sales_margin') }}
 JOIN 
 {{ ref('stg_raw__sales') }}
 USING (orders_id)
+GROUP BY orders_id,date_date
+ORDER BY orders_id
