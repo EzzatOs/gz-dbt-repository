eda862424f455e570af53fa79d1306a05b4404e5
ezzat_o@icloud.com <ezzat_o@icloud.com>
2026-07-11 15:48:25 +0000

    doing corrections

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,9 +1,1 @@
-select
-    sa.revenue,
-    sa.quantity,
-    pdt.purchase_price,
-    (sa.quantity * pdt.purchase_price) as purchase_cost,
-    (sa.revenue - (sa.quantity * pdt.purchase_price)) as margin
-from {{ ref("stg_raw_sales") }} as sa
-left join {{ ref("stg_raw_product") }} as pdt 
-on sa.products_id = pdt.products_id
+select 
