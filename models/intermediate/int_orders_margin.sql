9b2fbc6a3749a79940a4c853b911bec778c3be0a
ezzat_o@icloud.com <ezzat_o@icloud.com>
2026-07-12 13:04:37 +0000

    do some amendment

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,12 +1,15 @@
 SELECT 
 orders_id,
 ROUND(SUM(revenue),2) AS total_revenue,
-SUM(quantity) AS quantity,
-ROUND(SUM(purchase_cost),2) AS purchase_cost,
+revenue,
+SUM(quantity) AS total_quantity,
+quantity,
+ROUND(SUM(purchase_cost),2) AS total_purchase_cost,
+purchase_cost,
 ROUND(SUM(margin),2) AS margin
 FROM 
 {{ ref('int_sales_margin') }}
 JOIN 
-{{ ref('stg_raw__sales') }} AS s
+{{ ref('stg_raw__sales') }} 
 USING (orders_id)
 GROUP BY orders_id
