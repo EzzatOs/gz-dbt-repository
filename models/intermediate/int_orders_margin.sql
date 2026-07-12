29e618a4ecf82a2dd510d8211b62ab4d0172d363
ezzat_o@icloud.com <ezzat_o@icloud.com>
2026-07-12 10:15:31 +0000

    correct error

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,13 +1,12 @@
 SELECT 
 orders_id,
 SUM(revenue) AS total_revenue,
-date_date,
 SUM(quantity) AS quantity,
 SUM(purchase_cost) AS purchase_cost,
 SUM(margin) AS margin
 FROM 
 {{ ref('int_sales_margin') }}
 JOIN 
-{{ ref('stg_raw__sales') }}
+{{ ref('stg_raw__sales') }} AS s
 USING (orders_id)
-GROUP BY orders_id,date_date
+GROUP BY orders_id
