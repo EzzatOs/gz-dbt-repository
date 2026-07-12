a3cf13558a5126d5ad3f630a13a6733931d813ff
ezzat_o@icloud.com <ezzat_o@icloud.com>
2026-07-12 13:33:07 +0000

    generate finance records

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,17 +1,17 @@
 SELECT 
-date_date,
-COUNT(orders_id) AS total_transactions,
-SUM(revenue) AS total_revenue,
-AVG(SUM(revenue), COUNT(orders_id)) AS avg_basket,
-Operational_margin,
-SUM(purchase_cost) AS total_purchase_cost,
-SUM(shipping_fee) AS total_shipping_fees,
-SUM(logcost) AS total_logcost,
-SUM(quantity) AS total_quantity_sold
+im.date_date,
+COUNT(DISTINCT im.orders_id) AS total_transactions,
+SUM(im.revenue) AS total_revenue,
+SAFE_DIVIDE(SUM(im.revenue), COUNT(DISTINCT im.orders_id)) AS avg_basket,
+AVG(im.purchase_cost) AS avg_purchase_cost,
+AVG(oo.shipping_fee) AS avg_shipping_fee,
+AVG(oo.logcost) AS avg_logcost,
+SUM(pd.quantity) AS total_quantity_sold,
+AVG(oo.Operational_margin) AS avg_operational_margin
 FROM 
-{{ ref('int_sales_margin') }}
-JOIN {{ ref('int_orders_margin') }}
+{{ ref('int_sales_margin') }} AS im
+JOIN {{ ref('int_orders_margin') }} AS pd
 USING(orders_id)
-JOIN {{ ref('int_orders_operational') }}
+JOIN {{ ref('int_orders_operational') }} AS oo
 USING(orders_id)
-GROUP BY date_date, Operational_margin;
+GROUP BY date_date
