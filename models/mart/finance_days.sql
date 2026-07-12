8369d128fb7cb05e04a40dc7091349abcec86f27
ezzat_o@icloud.com <ezzat_o@icloud.com>
2026-07-12 13:22:09 +0000

    generate finance_days figures

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,7 +1,17 @@
-SELECT *
+SELECT 
+date_date,
+COUNT(orders_id) AS total_transactions,
+SUM(revenue) AS total_revenue,
+AVG(SUM(revenue), COUNT(orders_id)) AS avg_basket,
+Operational_margin,
+SUM(purchase_cost) AS total_purchase_cost,
+SUM(shipping_fee) AS total_shipping _fees,
+SUM(logcost) AS total_logcost,
+SUM(quantity) AS total_quantity_sold
 FROM 
 {{ ref('int_sales_margin') }}
 JOIN {{ ref('int_orders_margin') }}
 USING(orders_id)
 JOIN {{ ref('int_orders_operational') }}
 USING(orders_id)
+GROUP BY date_date, Operational_margin;
