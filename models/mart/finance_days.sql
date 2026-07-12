92b9ba81b8637e8dfce5adf04bba95685a71fe62
ezzat_o@icloud.com <ezzat_o@icloud.com>
2026-07-12 14:05:02 +0000

    done some amendment

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,13 +1,13 @@
 SELECT 
 im.date_date,
 COUNT(DISTINCT im.orders_id) AS total_transactions,
-SUM(pd.total_revenue) AS total_revenue,
-SAFE_DIVIDE(SUM(pd.total_revenue), COUNT(DISTINCT im.orders_id)) AS avg_basket,
-AVG(pd.purchase_cost) AS avg_purchase_cost,
-AVG(oo.shipping_fee) AS avg_shipping_fee,
-AVG(oo.logcost) AS avg_logcost,
+ROUND(SUM(pd.total_revenue),2) AS total_revenue,
+ROUND(SAFE_DIVIDE(SUM(pd.total_revenue), COUNT(DISTINCT im.orders_id)),2) AS avg_basket,
+ROUND(AVG(pd.purchase_cost),2) AS avg_purchase_cost,
+ROUND(AVG(oo.shipping_fee),2) AS avg_shipping_fee,
+ROUND(AVG(oo.logcost),2) AS avg_logcost,
 SUM(pd.quantity) AS total_quantity_sold,
-AVG(oo.Operational_margin) AS avg_operational_margin
+ROUND(AVG(oo.Operational_margin),2) AS avg_operational_margin
 FROM 
 {{ ref('int_sales_margin') }} AS im
 JOIN {{ ref('int_orders_margin') }} AS pd
