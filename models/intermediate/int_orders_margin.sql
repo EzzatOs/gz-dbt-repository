97df6c0b4aebc73dd66a07c04987070e90cd5101
ezzat_o@icloud.com <ezzat_o@icloud.com>
2026-07-12 14:31:21 +0000

    correction

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,15 +1,16 @@
 SELECT 
-orders_id,
-ROUND(SUM(revenue),2) AS total_revenue,
-revenue,
-SUM(quantity) AS total_quantity,
-quantity,
-ROUND(SUM(purchase_cost),2) AS total_purchase_cost,
-purchase_cost,
-ROUND(SUM(margin),2) AS margin
+s1.orders_id,
+s1.date_date,
+ROUND(SUM(s.revenue),2) AS total_revenue,
+s.revenue,
+SUM(s.quantity) AS total_quantity,
+s.quantity,
+ROUND(SUM(s1.purchase_cost),2) AS total_purchase_cost,
+s1.purchase_cost,
+ROUND(SUM(s1.margin),2) AS margins
 FROM 
-{{ ref('int_sales_margin') }}
+{{ ref('int_sales_margin') }} AS s1
 JOIN 
-{{ ref('stg_raw__sales') }} 
+{{ ref('stg_raw__sales') }}  AS s
 USING (orders_id)
-GROUP BY orders_id,revenue,quantity,purchase_cost
+GROUP BY s1.orders_id,s.revenue,s.quantity,s1.purchase_cost,s1.date_date
