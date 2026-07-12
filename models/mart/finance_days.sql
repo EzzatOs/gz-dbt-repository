143818bcdf6dad61f4cf2f9197ac46f25633abb1
ezzat_o@icloud.com <ezzat_o@icloud.com>
2026-07-12 13:37:16 +0000

    done some amendment

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,8 +1,8 @@
 SELECT 
 im.date_date,
 COUNT(DISTINCT im.orders_id) AS total_transactions,
-SUM(im.revenue) AS total_revenue,
-SAFE_DIVIDE(SUM(im.revenue), COUNT(DISTINCT im.orders_id)) AS avg_basket,
+SUM(pd.revenue) AS total_revenue,
+SAFE_DIVIDE(SUM(pd.revenue), COUNT(DISTINCT im.orders_id)) AS avg_basket,
 AVG(pd.purchase_cost) AS avg_purchase_cost,
 AVG(oo.shipping_fee) AS avg_shipping_fee,
 AVG(oo.logcost) AS avg_logcost,
