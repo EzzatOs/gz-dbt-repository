e4f76b97a845587178d74e545c3ed5b1930fc780
Ezzat Osman <ezzat_o@icloud.com>
2026-07-16 16:42:05 +0000

    aggregation

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,4 +1,7 @@
-SELECT *
+SELECT date_date, 
+SUM(ads_cost) AS ads_cost,
+SUM(click) AS click,
+SUM(impression) AS impression
 FROM {{ ref('int_campaigns') }}
 GROUP BY date_date
 ORDER BY date_date DESC
