2ee7420146a8d3fdd6ed9799885a422dd4db58c5
Ezzat Osman <ezzat_o@icloud.com>
2026-08-13 12:30:20 +0000

    correction

--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -10,7 +10,8 @@ ROUND(AVG(purchase_cost),2) AS avg_purchase_cost,
 ROUND(AVG(shipping_fee),2) AS avg_shipping_fee,
 ROUND(AVG(logcost),2) AS avg_logcost,
 SUM(total_quantity) AS total_quantity_sold,
-ROUND(AVG(Operational_margin),2) AS avg_operational_margin
+ROUND(AVG(Operational_margin),2) AS avg_operational_margin,
+Operational_margin
 FROM 
  {{ ref('int_orders_operational') }} 
 GROUP BY date_date
